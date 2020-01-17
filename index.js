d19f140fef5a3385f8579fcfd420760a621322c9
Doris Le <dorisle@sandiego.edu>
2020-01-17 19:05:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,13 +7,12 @@ for (var i=0; i < musicians.length; i++) {
 return allBeatles;
 }
 
-function johnLennonFacts() {
-    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-    var newFacts = [];
-    var i = 0;
-    while (i < facts.length) {
-        newFacts.push(facts[i] +"!!!");
-        i++;
-    }
-    return newFacts;
+function johnLennonFacts(facts) {
+  var results = [];
+  var count = 0;
+  while (count < facts.length) {
+    results.push(facts[count] + "!!!");
+    count++;
+  }
+  return results;
 }
